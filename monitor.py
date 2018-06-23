945a562251c10199f1414dddb244550de6c0748b
Rahul Arulkumaran <rahuldravid313@gmail.com>
2018-06-24 04:14:17 +0530

    Basic stuff added

---
+++ monitor.py
@@ -0,0 +1,41 @@
+import json
+import requests as r
+import time
+class MonitorPool:
+	def __init__(self):
+		self.url = "http://107.173.118.210/api/"
+
+	def send_request(self, url):
+		return r.get(url)
+
+	def get_json(self, req_data):
+		return req_data.json()
+
+	def get_stats(self, choice):
+		url = ""
+		if(choice == 1):
+			url += self.url + "stats"
+		elif(choice == 2):
+			url += self.url + "pool_stats"
+		elif(choice == 3):
+			url += self.url + "live_stats"
+		elif(choice == 4):
+			url += self.url + "getblocksstats"
+		else:
+			print("Wrong option entered!")
+			return
+
+		req_data = self.send_request(url)
+		json_obj = self.get_json(req_data)
+		return json_obj
+
+
+
+if(__name__=="__main__"):
+	while(True):
+		mp = MonitorPool()
+		d = mp.get_stats(1)
+
+		print(d)
+
+		time.sleep(1)
