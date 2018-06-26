ed858b0ea62089d280da1721e88310a584a1a464
rahulkumaran <rahuldravid313@gmail.com>
2018-06-26 05:30:49 +0530

    removed all choices excepts /stats

--- monitor.py
+++ monitor.py
@@ -1,8 +1,7 @@
 import json
 import requests as r
 import time
-from datetime import datetime
-
+import logging
 
 class MonitorPool:
 	def __init__(self, choice=1):
@@ -19,12 +18,12 @@ class MonitorPool:
 		url = ""
 		if(self.choice == 1):
 			url += self.url + "stats"
-		elif(self.choice == 2):
+		'''elif(self.choice == 2):
 			url += self.url + "pool_stats"
 		elif(self.choice == 3):
 			url += self.url + "live_stats"
 		elif(self.choice == 4):
-			url += self.url + "getblocksstats"
+			url += self.url + "getblocksstats"'''
 		else:
 			print("Wrong option entered!")
 			return
@@ -37,7 +36,11 @@ class MonitorPool:
 
 	def get_stats_data(self):
 		stats_json= self.get_stats_json()
+		#print(time.ctime(stats_json["time"]))
+		print(json.dumps(stats_json, indent=2))
 		if(self.choice==1):
+			
+			
 			print("Total Paid:",stats_json["pools"]["litecoin"]["poolStats"]["totalPaid"])
 			print("Valid Blocks:",stats_json["pools"]["litecoin"]["poolStats"]["validBlocks"])
 			print("Orphaned:",stats_json["pools"]["litecoin"]["blocks"]["orphaned"])
@@ -47,13 +50,26 @@ class MonitorPool:
 			print("Pending:",stats_json["pools"]["litecoin"]["blocks"]["pending"])
 
 			print("Hashrate:",stats_json["pools"]["litecoin"]["hashrate"])
-		#print(stats_json)
+			print("----------------------------------------------------------")
+
+			logging.basicConfig(filename="stats.log", level=logging.DEBUG)
+			logging.info(time.ctime(stats_json["time"]))
+			logging.info("Total Paid: "+str(stats_json["pools"]["litecoin"]["poolStats"]["totalPaid"]))
+			logging.info("Valid Blocks: "+str(stats_json["pools"]["litecoin"]["poolStats"]["validBlocks"]))
+			logging.info("Orphaned: "+str(stats_json["pools"]["litecoin"]["blocks"]["orphaned"]))
+			logging.info(" Confirmed: "+str(stats_json["pools"]["litecoin"]["blocks"]["confirmed"]))
+			logging.info("Pending: "+str(stats_json["pools"]["litecoin"]["blocks"]["pending"]))
+			logging.info("Hashrate: "+str(stats_json["pools"]["litecoin"]["hashrate"]))
+			logging.info("----------------------------------------")
+
+
+		
 
 if(__name__=="__main__"):
 	while(True):
-		mp = MonitorPool()
+
+		mp = MonitorPool(2)
 		d = mp.get_stats_data()
+		time.sleep(900)
 
-		#print(json.dumps(d, indent = 2))
-
-		time.sleep(60)
+		
