393257db4d3c9d568ca2f8aaf55878316cfb2de5
rahulkumaran <rahuldravid313@gmail.com>
2018-06-29 23:41:35 +0530

    changed requests url and crypto type

--- src/monitor.py
+++ src/monitor.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 class MonitorPool:
 	def __init__(self, choice=1):
-		self.url = "http://107.173.118.210/api/"
+		self.url = "http://107.173.118.210:8080/api/"
 		self.choice = choice
 
 	def send_request(self, url):
@@ -37,25 +37,25 @@ class MonitorPool:
 		if(self.choice==1):
 			
 			print(time.ctime(stats_json["time"]))
-			print("Total Paid:",stats_json["pools"]["litecoin"]["poolStats"]["totalPaid"])
-			print("Valid Blocks:",stats_json["pools"]["litecoin"]["poolStats"]["validBlocks"])
-			print("Orphaned:",stats_json["pools"]["litecoin"]["blocks"]["orphaned"])
+			print("Total Paid:",stats_json["pools"]["zen"]["poolStats"]["totalPaid"])
+			print("Valid Blocks:",stats_json["pools"]["zen"]["poolStats"]["validBlocks"])
+			print("Orphaned:",stats_json["pools"]["zen"]["blocks"]["orphaned"])
 
-			print("Confirmed:",stats_json["pools"]["litecoin"]["blocks"]["confirmed"])
+			print("Confirmed:",stats_json["pools"]["zen"]["blocks"]["confirmed"])
 
-			print("Pending:",stats_json["pools"]["litecoin"]["blocks"]["pending"])
+			print("Pending:",stats_json["pools"]["zen"]["blocks"]["pending"])
 
-			print("Hashrate:",stats_json["pools"]["litecoin"]["hashrate"])
+			print("Hashrate:",stats_json["pools"]["zen"]["hashrate"])
 			print("----------------------------------------------------------")
 
 			logging.basicConfig(filename="stats.log", level=logging.DEBUG)
 			logging.info(time.ctime(stats_json["time"]))
-			logging.info("Total Paid: "+str(stats_json["pools"]["litecoin"]["poolStats"]["totalPaid"]))
-			logging.info("Valid Blocks: "+str(stats_json["pools"]["litecoin"]["poolStats"]["validBlocks"]))
-			logging.info("Orphaned: "+str(stats_json["pools"]["litecoin"]["blocks"]["orphaned"]))
-			logging.info(" Confirmed: "+str(stats_json["pools"]["litecoin"]["blocks"]["confirmed"]))
-			logging.info("Pending: "+str(stats_json["pools"]["litecoin"]["blocks"]["pending"]))
-			logging.info("Hashrate: "+str(stats_json["pools"]["litecoin"]["hashrate"]))
+			logging.info("Total Paid: "+str(stats_json["pools"]["zen"]["poolStats"]["totalPaid"]))
+			logging.info("Valid Blocks: "+str(stats_json["pools"]["zen"]["poolStats"]["validBlocks"]))
+			logging.info("Orphaned: "+str(stats_json["pools"]["zen"]["blocks"]["orphaned"]))
+			logging.info(" Confirmed: "+str(stats_json["pools"]["zen"]["blocks"]["confirmed"]))
+			logging.info("Pending: "+str(stats_json["pools"]["zen"]["blocks"]["pending"]))
+			logging.info("Hashrate: "+str(stats_json["pools"]["zen"]["hashrate"]))
 			logging.info("----------------------------------------")
 
 		return
