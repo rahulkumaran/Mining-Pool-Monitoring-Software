127f01fc692cf94dd346f0bfedfd0f6169da2123
rahulkumaran <rahuldravid313@gmail.com>
2018-06-30 00:49:04 +0530

    changed time of sleep to 30 mins

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import time
 
 if(__name__=="__main__"):
 	while(True):
-
 		mp = MonitorPool()
 		stats_json = mp.get_stats_data()
 		mp.store_data_csv(stats_json)
-		time.sleep(0.1)
+		time.sleep(900)
 
